cfd4ca7fb0c45ad33dab534e1dda64cd460675dd
Gadzhi Gadzhiev <gadzhi@gadzhiev.com>
2013-08-11 00:06:52 +0400

    user_locale should be before_filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
-  around_filter :user_locale
+  before_filter :user_locale
   around_filter :user_time_zone,  if: :current_user
 
   def current_ability
