1f163b82629905543e875295c1dce78b8dc1589c
Gadzhi Gadzhiev <gadzhi@gadzhiev.com>
2013-08-26 11:42:34 +0400

    Fixed routing spec for messages

--- spec/routing/messages_routing_spec.rb
+++ spec/routing/messages_routing_spec.rb
@@ -4,31 +4,31 @@ describe MessagesController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/messages").should route_to("messages#index")
+      get("/rooms/1/messages").should route_to("messages#index", room_id: "1")
     end
 
     it "routes to #new" do
-      get("/messages/new").should route_to("messages#new")
+      get("/rooms/1/messages/new").should route_to("messages#new", room_id: "1")
     end
 
     it "routes to #show" do
-      get("/messages/1").should route_to("messages#show", :id => "1")
+      get("/rooms/1/messages/1").should route_to("messages#show", id: "1", room_id: "1")
     end
 
     it "routes to #edit" do
-      get("/messages/1/edit").should route_to("messages#edit", :id => "1")
+      get("/rooms/1/messages/1/edit").should route_to("messages#edit", id: "1", room_id: "1")
     end
 
     it "routes to #create" do
-      post("/messages").should route_to("messages#create")
+      post("/rooms/1/messages").should route_to("messages#create", room_id: "1")
     end
 
     it "routes to #update" do
-      put("/messages/1").should route_to("messages#update", :id => "1")
+      put("/rooms/1/messages/1").should route_to("messages#update", room_id: "1", id: "1")
     end
 
     it "routes to #destroy" do
-      delete("/messages/1").should route_to("messages#destroy", :id => "1")
+      delete("/rooms/1/messages/1").should route_to("messages#destroy", id: "1", room_id: "1")
     end
 
   end
