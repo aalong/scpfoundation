1cef5857427ac1790478317ac03ad899901e09dc
Gadzhi Gadzhiev <gadzhi@gadzhiev.com>
2013-10-16 23:21:22 +0400

    Fix DatabaseCleaner config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,18 +19,18 @@ RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :deletion
-    DatabaseCleaner.clean_with :deletion
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with :truncation
   end
 
   config.before(:each) do
-    DatabaseCleaner.start
     I18n.backend.reload!
     load "#{Rails.root}/db/seeds.rb"
+    DatabaseCleaner.start
   end
 
   config.after(:each) do
-    DatabaseCleaner.start
+    DatabaseCleaner.clean
   end
 
   config.include Devise::TestHelpers, type: :controller
